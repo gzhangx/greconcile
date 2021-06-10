72ea3101b4ddae427d2b4acf499da50dccf3136f
Gang Zhang <gzhangx@hotmail.com>
2021-06-09 23:02:09 -0400

    fix csv

--- maintenance/m.js
+++ maintenance/m.js
@@ -1,8 +1,64 @@
 const fs = require('fs');
 const moment = require('moment');
 
-const lines = fs.readFileSync('./MaintainessRecord.csv').toString().split('\n').map(r => {
-    const d = r.split(',');
+function parseCsvLine(str) {
+    const res = str.split('').reduce((acc, c) => {
+        if (!acc.inQuote) {
+            if (c === ',') {
+                acc.res.push(acc.cur);
+                acc.cur = '';
+                return acc;
+            }
+            if (c === '"') {
+                acc.inQuote = c;
+                return acc;
+            }
+            if (c === '\r') {
+                return acc;
+            }
+            if (c === '\n') {
+                if (acc.cur) {
+                    acc.res.push(acc.cur);
+                    acc.cur = '';
+                }
+                acc.allRess.push(acc.res);
+                acc.res = [];
+            }
+        } else {            
+            if (c === acc.inQuote) {
+                if (acc.inQuoteLastChar != acc.inQuote) {
+                    acc.inQuoteLastChar = c;
+                    return acc;
+                } else {
+                    acc.inQuoteLastChar = null;
+                    acc.cur += c;
+                    return acc;
+                }
+            }
+            if (acc.inQuoteLastChar == acc.inQuote) {
+                acc.inQuote = false;
+                acc.inQuoteLastChar = null;
+            }            
+        }
+        acc.cur += c;
+        return acc;
+    }, {
+        res: [],
+        allRess: [],
+        inQuote: false,
+        inQuoteLastChar: null,
+        cur: '',
+    });
+    if (res.cur) {
+        res.res.push(res.cur);
+    }
+    if (res.res.length) {
+        res.allRess.push(res.res);
+    }
+    return res.allRess;
+}
+const lines = parseCsvLine(fs.readFileSync('./MaintainessRecord.csv').toString()).map(d => {
+    console.log(d)
     let amount = d[1].trim().replace(/[$]/g,'').trim();
     return {
         date: moment(d[0]).format('YYYY-MM-DD'),
