8f47f9eeb857018ccf96a71e9c600d3bf3b00e8c
Gang Zhang <gzhangx@hotmail.com>
2021-06-09 23:10:15 -0400

    fix various

--- maintenance/m.js
+++ maintenance/m.js
@@ -3,42 +3,47 @@ const moment = require('moment');
 
 function parseCsvLine(str) {
     const res = str.split('').reduce((acc, c) => {
-        if (!acc.inQuote) {
-            if (c === ',') {
-                acc.res.push(acc.cur);
-                acc.cur = '';
-                return acc;
-            }
-            if (c === '"') {
-                acc.inQuote = c;
-                return acc;
-            }
-            if (c === '\r') {
-                return acc;
-            }
-            if (c === '\n') {
-                if (acc.cur) {
+        while (true) {
+            if (!acc.inQuote) {
+                if (c === ',') {
                     acc.res.push(acc.cur);
                     acc.cur = '';
-                }
-                acc.allRess.push(acc.res);
-                acc.res = [];
-            }
-        } else {            
-            if (c === acc.inQuote) {
-                if (acc.inQuoteLastChar != acc.inQuote) {
-                    acc.inQuoteLastChar = c;
                     return acc;
-                } else {
+                }
+                if (c === '"') {
+                    acc.inQuote = c;
+                    return acc;
+                }
+                if (c === '\r') {
+                    return acc;
+                }
+                if (c === '\n') {
+                    if (acc.cur) {
+                        acc.res.push(acc.cur);
+                        acc.cur = '';
+                    }
+                    acc.allRess.push(acc.res);
+                    acc.res = [];
+                    return acc;
+                }
+                break;
+            } else {
+                if (c === acc.inQuote) {
+                    if (acc.inQuoteLastChar != acc.inQuote) {
+                        acc.inQuoteLastChar = c;
+                        return acc;
+                    } else {
+                        acc.inQuoteLastChar = null;
+                        acc.cur += c;
+                        return acc;
+                    }
+                } else if (acc.inQuoteLastChar == acc.inQuote) {
+                    acc.inQuote = false;
                     acc.inQuoteLastChar = null;
-                    acc.cur += c;
-                    return acc;
+                    continue;
                 }
+                break;
             }
-            if (acc.inQuoteLastChar == acc.inQuote) {
-                acc.inQuote = false;
-                acc.inQuoteLastChar = null;
-            }            
         }
         acc.cur += c;
         return acc;
@@ -58,7 +63,7 @@ function parseCsvLine(str) {
     return res.allRess;
 }
 const lines = parseCsvLine(fs.readFileSync('./MaintainessRecord.csv').toString()).map(d => {
-    console.log(d)
+    //console.log(d)
     let amount = d[1].trim().replace(/[$]/g,'').trim();
     return {
         date: moment(d[0]).format('YYYY-MM-DD'),
@@ -67,4 +72,4 @@ const lines = parseCsvLine(fs.readFileSync('./MaintainessRecord.csv').toString()
         worker:d[3],
     }
 });
-console.log(lines)
+console.log(lines.map(l=>l.date+' '+l.amount+' '+l.cat))
